63a51d4816c1ffb20ea79c115f9dfa667d8b86b6
Gourab-Ghosh <64676296+Gourab-Ghosh@users.noreply.github.com>
2022-12-29 19:44:03 +0530

    Update main.py

--- archinstall/main.py
+++ archinstall/main.py
@@ -25,11 +25,11 @@ def main():
         sys.exit()
     swap_size = response["swap_to_file_size"]
     try:
-        swap_size_int = int(swap_size)
-    except:
+        swap_size_float = float(swap_size)
+    except ValueError:
         response["swap_to_file_size"] = None
     else:
-        response["swap_to_file_size"] = swap_size_int
+        response["swap_to_file_size"] = None if swap_size_float == float("inf") else swap_size_float
     passwords = ask_password(not response["username"])
     response.update(passwords)
     response["timezone"] = ask_timezone()
